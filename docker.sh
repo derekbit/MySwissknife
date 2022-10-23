e38d70657303d9fdc0eb148065c015812cb634ab
Derek Su <derek.su@suse.com>
2022-10-23 11:18:22 +0800

    docker.sh: docker installlation
    Signed-off-by: Derek Su <derek.su@suse.com>

---
+++ docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function install_docker
+{
+    sudo apt-get remove -y docker docker-engine docker.io containerd runc
+
+    sudo apt-get update -y
+    sudo apt-get install -y ca-certificates curl gnupg lsb-release
+
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update -y
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+    # Post-installation
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+}
+
+install_docker
